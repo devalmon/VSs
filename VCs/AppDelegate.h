7a0aa6ed4c81867846c0b3bf917b93ad3b9ab22c
Alexey B <phlood@yandex.ru>
2020-07-26 18:07:36 +0300

    Initial Commit

---
+++ VCs/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VCs
+//
+//  Created by Alexey Baryshnikov on 26.07.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ VCs/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  VCs
+//
+//  Created by Alexey Baryshnikov on 26.07.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
